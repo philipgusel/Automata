9f099e9f2af84ded97415fe421bf24b6f3a308a8
Philip Gusel <17913214+philipgusel@users.noreply.github.com>
2018-11-29 21:40:48 +0100

    Update Automata.hs

--- Automata.hs
+++ Automata.hs
@@ -1,26 +1,13 @@
 module Automata where
 
-
-type Delta = (Integer -> String -> Integer)
-type Endstates = [Integer]
-type Init = Integer
-
-type Automata = (Endstates,Init,Delta)
-
 element _ [] = False
 element a (x:xs)
 	| x == a = True
 	| otherwise = element a xs
 
-checkword :: Automata -> [String] -> Bool
 checkword (e,i,d) l  = checkwordrecur (e,i,d) l i
 
-checkwordrecur :: Automata -> [String] -> Integer -> Bool
 checkwordrecur (e,i,d) [] c 
         | c `element` e == True = True
         | otherwise = False
 checkwordrecur (e,i,d) (x:xs) c = checkwordrecur (e,i,d) xs (d c x)
-
-
-
-
